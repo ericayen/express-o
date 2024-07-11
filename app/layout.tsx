4b59e546d18da71ba1ef100461d18f64f5f6a58c
Erica Yen <eyen@Ericas-MacBook-Air.local>
2024-07-11 16:46:39 -0700

    add footer with basic styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import MainNav from "./ui/nav";
+import Footer from "./ui/footer";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -20,6 +21,7 @@ export default function RootLayout({
       <body className={inter.className}>
           <MainNav />
         <div className="flex-grow md:overflow-y-auto">{children}</div>
+        <Footer />
       </body>
     </html>
   );
